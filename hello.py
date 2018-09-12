f6442c94b510f3a094401aa20ee7378c749e7ac2
harebare01 <harebare01@gmail.com>
2018-09-12 13:38:51 +0900

    Update hello.py

--- hello.py
+++ hello.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/hi')
 def hello_world():
-    return '<h1>Hello, Build wiht git_post-commit 二階堂 World!</h1>'
+    return '<h1>Hello, Build wiht git_post-commit xxxx World!</h1>'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
